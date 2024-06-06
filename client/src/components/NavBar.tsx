4a9266fa305f29a9eeab2537788d5904d7283e44
Geethika <146985937+12kittu04@users.noreply.github.com>
2024-06-06 08:58:28 -0500

    Resolved merge conflicts

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,22 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Icon } from "@iconify/react";
 import { Link, useNavigate } from "react-router-dom";
-import { ButtonBase } from "@mui/material";
+import { ButtonBase, Drawer } from "@mui/material";
 import { useTheme } from "../context/ThemeContext";
-import { useAuth, UserType } from "../context/AuthContext";
+import { useAuth } from "../context/AuthContext";
+import { Dropdown, MenuProps } from "antd";
 
 interface NavBarPropsTypes {
   name: string;
 }
 
 const NavBar = (props: NavBarPropsTypes) => {
+  const [isDrawerOpen, openDrawer] = useState<boolean>(false);
   const { theme } = useTheme();
   const navigate = useNavigate();
-  const { isAuthenticated, logout, type } = useAuth();
-
-  const userIcon = type === UserType.Admin ? "mdi:account-tie" : "mdi:school";
-
-
+  const { isAuthenticated, logout, username, checkUserType } = useAuth();
 
   return (
     <>
@@ -27,28 +25,62 @@ const NavBar = (props: NavBarPropsTypes) => {
         <button className="absolute text-white sm:left-2 text-5xl hover:bg-blue-900/75 hover:cursor-pointer transition ease-in-out delay-75 rounded-md" onClick={() => navigate(-1)}>
           <Icon icon="material-symbols:arrow-left-alt-rounded" />
         </button>
+        <Icon
+          icon="fa6-solid:bars"
+          className="absolute px-2 text-white text-5xl left-14 hover:bg-blue-900/75 hover:cursor-pointer transition ease-in-out delay-75 rounded-md"
+          onClick={() => openDrawer(true)}
+        />
         <div className="navbar-title sm:leading-loose text-center text-white font-bold sm:text-4xl text-3xl my-0 mx-auto max-sm:w-1/2 max-sm:leading-10">
           {props.name}
         </div>
-        <Icon
-          icon={userIcon}
-          className="absolute p-1 text-white text-5xl top-[20%] sm:right-[7vw] hover:bg-blue-900/75 hover:cursor-pointer transition ease-in-out delay-75 rounded-md"
-        /> <div className="home-icon">
-        <Link to="/home">
-          <Icon
-            icon="material-symbols:home"
-            className="absolute p-1 text-white text-5xl top-[20%] sm:right-[4svw] hover:bg-blue-900/75 hover:cursor-pointer transition ease-in-out delay-75 rounded-md"
-          />
-        </Link>
-      </div>
-        {isAuthenticated && (
+        <div className="user-info group absolute sm:top-[55%] sm:-translate-y-[55%] sm:right-[1.25svw] sm:p-3 flex sm:gap-x-2 sm:py-2 sm:px-3 border-2 border-solid border-white rounded-xl drop-shadow-xl hover:bg-[#2F528F] hover:cursor-pointer transition delay-75">
+          <div className="text-white">
+            <div className="sm:max-w-1/2 truncate">{username}</div>
+            <div className="text-right">{checkUserType() === "admin" ? "Admin" : "Student"}</div>
+          </div>
+          <img src="/user.png" alt="" className="sm:w-[42px] sm:h-[42px]"/>
+          <Icon icon="bxs:left-arrow" className="text-white self-center group-hover:-rotate-90 transition duration-150"/>
+          <div className="user-info-actions absolute w-full bg-white sm:py-2 sm:px-3 hidden group-hover:flex group-focus:flex flex-col top-full left-0 sm:gap-y-2 rounded-lg sm:translate-y-2 before:content-[''] before:absolute before:left-0 before:-top-2 before:w-full before:h-2">
+            <div className="flex sm:gap-x-2 text-black items-center justify-center hover:bg-black/30 hover:cursor-pointer transition delay-75 sm:min-h-8 rounded-lg" onClick={() => {
+              navigate("/home");
+            }}>
+              <div>Home</div>
+              <Icon icon="material-symbols:home" />
+            </div>
+            <div className="flex sm:gap-x-2 text-black items-center justify-center hover:bg-black/30 hover:cursor-pointer transition delay-75 sm:min-h-8 rounded-lg" onClick={() => {
+              logout();
+              navigate("/login");
+            }}>
+              <div>Logout</div>
+              <Icon icon="mdi:logout" />
+            </div>
+          </div>
+        </div>
+        <div className="home-icon">
+          <Link to="/home">
+            <Icon
+              icon="material-symbols:home"
+              className="absolute p-1 text-white text-5xl sm:top-1/2 sm:-translate-y-1/2 sm:right-[13.5svw] hover:bg-blue-900/75 hover:cursor-pointer transition ease-in-out delay-75 rounded-md"
+            />
+          </Link>
+        </div>
+        {/* {isAuthenticated && (
           <div className="logout-icon">
             <Icon icon="mdi:logout" className="absolute text-white sm:text-5xl sm:right-[1.25vw] sm:top-1/2 -translate-y-1/2 hover:bg-blue-900/75 hover:cursor-pointer transition ease-in-out delay-75 rounded-md p-1" onClick={() => {
               logout();
               navigate("/login");
             }}/>
           </div>
-        )}
+        )} */}
+        <Drawer
+          anchor='left'
+          open={isDrawerOpen}
+          onClose={() => openDrawer(false)}
+        >
+          <div className="drawer-container sm:w-[15svw] sm:h-full bg-[#CFD5EA] flex flex-col items-center py-4 sm:space-y-6">
+            
+          </div>
+        </Drawer>
       </div>
     </>
   );
