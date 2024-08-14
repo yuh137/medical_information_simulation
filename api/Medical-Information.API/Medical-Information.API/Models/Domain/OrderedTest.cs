feced7c63a0d47ec9f4ce0349e4c3b75c7042fdd
HUY <nquochuy137@gmail.com>
2024-08-14 09:20:45 -0500

    added schema to save student input tests

--- api/Medical-Information.API/Medical-Information.API/Models/Domain/OrderedTest.cs
+++ api/Medical-Information.API/Medical-Information.API/Models/Domain/OrderedTest.cs
@@ -1,16 +1,26 @@
 ﻿using Medical_Information.API.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Medical_Information.API.Models.Domain
 {
-    public class AdminQCLot
+    public class OrderedTest
     {
-        public Guid AdminQCLotID { get; set; }
+        [Key]
+        public Guid TestID { get; set; }
+        [Required]
         public string LotNumber { get; set; }
+        [Required]
         public DateTime OpenDate { get; set; }
         public DateTime? ClosedDate { get; set; }
+        [Required]
         public DateTime ExpirationDate { get; set; }
         public DateTime FileDate { get; set; }
+        [Required]
         public Department Department { get; set; }
+        [ForeignKey("Student")]
+        public Guid StudentID { get; set; }
         public ICollection<Analyte> Analytes { get; set; } = [];
+        public ICollection<double>? AnalyteValues { get; set; } = [];
     }
 }
