1d8c3f0d1ce92dccc883351aab2c531dd95ec1f3
JackCorwin <jackcorwin@outlook.com>
2024-10-23 18:01:09 -0500

    jack more changes new

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -29,6 +29,7 @@ import SimpleAnalyteInputPage from "./pages/General/Chemistry/SimpleAnalyteInput
 import Simple_Faculty_QC_Review  from "./pages/FacultyView/Simple_Faculty_Review_Controls";
 import MicrobiologyQCBuilder from "./pages/General/Microbiology/MicrobiologyQCBuilderPage";
 import MicrobiologyEditQC from "./pages/General/Microbiology/MicrobiologyEditQCPage";
+import MicrobiologyTestInput from "./pages/General/Microbiology/MicrobiologyTestInputPage";
 import { getAllDataFromStore } from "./utils/indexedDB/getData";
 
 function App() {
@@ -155,6 +156,10 @@ function AppWithRouter() {
               {
                 path: "edit_qc",
                 element: <MicrobiologyEditQC />,
+              },
+              {
+                path: "test_input/:item",
+                element: <MicrobiologyTestInput />,
               }
             ]
           },
