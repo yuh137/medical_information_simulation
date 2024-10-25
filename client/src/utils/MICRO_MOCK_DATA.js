af87edcc6bd4eef2cf3abb4edd3db53b8ea92380
JackCorwin <jackcorwin@outlook.com>
2024-10-24 19:39:41 -0500

    made the MICRO_MOCK_DATA page

---
+++ client/src/utils/MICRO_MOCK_DATA.js
@@ -0,0 +1,12 @@
+export const MMD = [
+    {
+      analyteName: "Catalase Negative",
+      analyteAcronym: "Catalase-Neg",
+      expextedRange: "Negative-No Bubbles"
+    },
+    {
+       analyteName: "Catalase Postive",
+       analyteAcronym: "Catalase-Pos",
+       expextedRange: "Positive-Bubbles"
+    }
+]
