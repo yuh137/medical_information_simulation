378fa7f5d4999bd053ff961c5d3847ae16c0e469
Gojofficial <lawezema@gmail.com>
2024-10-25 10:35:44 -0500

    Started with our mock data

---
+++ api/Medical-Information.API/Medical-Information.API/Models/Domain/AntiSera.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Medical_Information.API.Models.Domain
+{
+    public class AntiSera
+    {
+        [Key]
+        public Guid AntiSeraID { get; set; }
+        public string AntiSeraName { get; set; }
+        public string Abbreviation { get; set; }
+        public string ImmediateSpinRange { get; set; }
+        public string DegreeRange { get; set; }
+        public string AHQRange { get; set; }
+        public string CheckCellsRange { get; set; }
+        [ForeignKey("AdminQCLotID")]
+        public Guid AdminQCLotID { get; set; }
+        //public AdminQCLot AdminQCLot { get; set; }
+    }
+}
