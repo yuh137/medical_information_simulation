485ba86205df939f48c2815a389a9e396e7afd53
Colby-Coffman <colbycoffman20@gmail.com>
2024-10-25 19:33:11 -0500

    W

--- client/src/utils/DNALYTICS_DEBUG_UTIL.ts
+++ client/src/utils/DNALYTICS_DEBUG_UTIL.ts
@@ -2,23 +2,14 @@ interface MolecularQCPanel {
 	fileName: string;
 }
 
-export async function DEBUG_add_molecular_data_to_idb(QCPanels: string[]): void {
-	try {
-		await DEBUG_ADD(QCPanels);
-		console.log("Insert Succeeded");
-	} catch (error) {
-		console.error("Insert Failed", error);
-	}
-}
-
-function DEBUG_ADD(QCPanels: string[]): Promise<void> {
+export async function DEBUG_add_molecular_data_to_idb(QCPanels: string[]): Promise<void> {
 	return new Promise((resolve, reject) => {
 		const request = indexedDB.open("MIS_database");
 		request.onsuccess = (event) => {
 			const db = (event.target as IDBOpenDBRequest).result;
 			const transaction = db.transaction("qc_store", "readwrite");
 			const objectStore = transaction.objectStore("qc_store");
-			const addRequest = objectStore.add(QCPanels.map((item) => { fileName: item });
+			const addRequest = objectStore.add(QCPanels.map((item) => { fileName: item }));
 			addRequest.onsuccess = () => {
 				console.log("Added idb data");
 				resolve();
@@ -40,4 +31,7 @@ function DEBUG_ADD(QCPanels: string[]): Promise<void> {
 			reject(request.error);
 		};
 	});
+}
+
+function DEBUG_ADD(QCPanels: string[]):  {
 }
