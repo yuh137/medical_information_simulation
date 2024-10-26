518785a251c51abd08353661c33b828013f8e5c6
Colby-Coffman <colbycoffman20@gmail.com>
2024-10-26 18:27:58 -0500

    W

--- client/src/pages/General/Molecular/MolecularQCResult.tsx
+++ client/src/pages/General/Molecular/MolecularQCResult.tsx
@@ -1,6 +1,7 @@
 import NavBar from "../../../components/NavBar";
 import React, { useState, useEffect } from "react";
 import { getAllDataByFileName, getQCRangeByDetails } from "../../../utils/indexedDB/getData";
+import { Table, TableBody, TableCell, TableHead, TableRow, TableHeader } from "../../../components/ui/table";
 import {
   ColumnDef,
   useReactTable,
@@ -17,9 +18,9 @@ interface QCItem {
 
 const columns: ColumnDef<QCItem>[] = [
   {
-    accessorKey: "department",
+    id: 'departmentColumn',
     header: () => <span>Department</span>,
-    cell: info => info.getValue(),
+    cell: () => <span>Molecular</span>
   },
   {
     accessorKey: "fileName",
@@ -30,6 +31,7 @@ const columns: ColumnDef<QCItem>[] = [
 
 const MolecularQCResult = () => {
   const [qcData, setQcData] = useState<QCItem[]>([]);
+  const [selectedQC, setSelectedQC] = useState<QCItem | null>(null);
 
   const fetchQCData = async () => {
 // TODO(colby): DEBUG
@@ -42,10 +44,7 @@ const MolecularQCResult = () => {
     console.log("Fetching QC data...");
     const selectedQCs: string[] = JSON.parse(localStorage.getItem('selectedQCItems') || '[]');
     const allDataPromises = selectedQCs.map(qcName => getAllDataByFileName("qc_store", qcName));
-    const promise_results = await Promise.all(allDataPromises);
-// TODO(colby): MAYBE DEBUG
- //   add department key containing Molecular
-    const results = promise_results.map(result => result.department = 'Molecular');
+    const results = await Promise.all(allDataPromises);
     setQcData(results.flat());
     console.log("Fetched QC data:", results.flat());
   };
@@ -55,9 +54,58 @@ const MolecularQCResult = () => {
     fetchQCData();
   }, []);
 
+  const table = useReactTable({
+    data: qcData,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+  });
+
   return (
     <>
       <NavBar name={`Molecular QC Results`} />
+      <div className="relative">
+        <div className="table-container flex flex-col mt-8 sm:max-w-[75svw] sm:max-h-[75svh] sm:mx-auto w-100svw bg-[#CFD5EA]">
+          <Table className="p-8 rounded-lg border-solid border-[1px] border-slate-200">
+            <TableHeader>
+              {table.getHeaderGroups().map(group => (
+                <TableRow key={group.id} className="font-bold text-3xl bg-[#3A62A7] hover:bg-[#3A62A7] sticky top-0">
+                  {group.headers.map(header => (
+                    <TableHead
+                      key={header.id}
+                      className="text-white text-center"
+                    >
+                      {flexRender(header.column.columnDef.header, header.getContext())}
+                    </TableHead>
+                  ))}
+                </TableRow>
+              ))}
+            </TableHeader>
+            <TableBody>
+              {table.getRowModel().rows.map(row => (
+                <TableRow
+                  key={row.id}
+                  onClick={() => setSelectedQC(row.original)}
+                  style={{ backgroundColor: row.original === selectedQC ? '#3A62A7' : undefined, color: row.original === selectedQC ? 'white' : undefined }}
+                >
+                  {row.getVisibleCells().map(cell => (
+                    <TableCell key={cell.id}>
+                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))}
+              {qcData.length === 0 && (
+                <TableRow>
+                  <TableCell colSpan={3} style={{ textAlign: 'center' }}>
+                    No data available
+                  </TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>  
+        </div>
+      </div>
     </>
   );
 };
