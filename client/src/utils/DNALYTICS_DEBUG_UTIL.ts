e8608c18de485f9e560cf8a572a440e0375fe70f
Colby-Coffman <colbycoffman20@gmail.com>
2024-10-25 19:26:43 -0500

    W

---
+++ client/src/utils/DNALYTICS_DEBUG_UTIL.ts
@@ -0,0 +1,43 @@
+interface MolecularQCPanel {
+	fileName: string;
+}
+
+export async function DEBUG_add_molecular_data_to_idb(QCPanels: string[]): void {
+	try {
+		await DEBUG_ADD(QCPanels);
+		console.log("Insert Succeeded");
+	} catch (error) {
+		console.error("Insert Failed", error);
+	}
+}
+
+function DEBUG_ADD(QCPanels: string[]): Promise<void> {
+	return new Promise((resolve, reject) => {
+		const request = indexedDB.open("MIS_database");
+		request.onsuccess = (event) => {
+			const db = (event.target as IDBOpenDBRequest).result;
+			const transaction = db.transaction("qc_store", "readwrite");
+			const objectStore = transaction.objectStore("qc_store");
+			const addRequest = objectStore.add(QCPanels.map((item) => { fileName: item });
+			addRequest.onsuccess = () => {
+				console.log("Added idb data");
+				resolve();
+			};
+
+			addRequest.onerror = () => {
+				console.log("Error adding data", addRequest.error);
+				reject(addRequest.error);
+			};
+
+			transaction.oncomplete = () => {
+				db.close()
+			};
+
+		};
+
+		request.onerror = () => {
+			console.error("Error opening database", request.error);
+			reject(request.error);
+		};
+	});
+}
