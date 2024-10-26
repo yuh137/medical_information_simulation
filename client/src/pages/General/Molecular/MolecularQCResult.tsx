c53c8c14e3bbb038953c8051efb043298b56c26f
Colby-Coffman <colbycoffman20@gmail.com>
2024-10-25 19:51:53 -0500

    W

--- client/src/pages/General/Molecular/MolecularQCResult.tsx
+++ client/src/pages/General/Molecular/MolecularQCResult.tsx
@@ -1,5 +1,6 @@
 import NavBar from "../../../components/NavBar";
 import React, { useState, useEffect } from "react";
+import { getAllDataByFileName, getQCRangeByDetails } from "../../../utils/indexedDB/getData";
 import {
   ColumnDef,
   useReactTable,
--- client/src/utils/DNALYTICS_DEBUG_UTIL.ts
+++ client/src/utils/DNALYTICS_DEBUG_UTIL.ts
@@ -9,7 +9,7 @@ export async function DEBUG_add_molecular_data_to_idb(QCPanels: string[]): Promi
 			const db = (event.target as IDBOpenDBRequest).result;
 			const transaction = db.transaction("qc_store", "readwrite");
 			const objectStore = transaction.objectStore("qc_store");
-			const addRequest = objectStore.add(QCPanels.map((item) => { fileName: item }));
+			const addRequest = objectStore.add(QCPanels.map((item) => { return { fileName: item }}));
 			addRequest.onsuccess = () => {
 				console.log("Added idb data");
 				resolve();
