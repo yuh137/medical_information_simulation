29020b70e7d1ad372980c1fb17fedbab4c5fc69a
Colby-Coffman <colbycoffman20@gmail.com>
2024-10-26 19:21:47 -0500

    W

--- client/src/utils/DNALYTICS_DEBUG_UTIL.ts
+++ client/src/utils/DNALYTICS_DEBUG_UTIL.ts
@@ -18,8 +18,8 @@ export async function DEBUG_add_molecular_data_to_idb(QCPanels: string[]): Promi
 			const items = QCPanels.map((item) => { return { fileName: item, lotNumber: getNextID(), closedDate: getNextID() }})
 
 			let completedRequests = 0;
-			const getRequests = items.map((item) => objectStore.get([item.fileName, item.lotNumber, item.closedDate]));
-			getRequests.forEach(getRequest => {
+            items.forEach((item) => {
+                const getRequest = objectStore.get([item.fileName, item.lotNumber, item.closedDate]));
 				getRequest.onsuccess = (event) => {
                     if ((event.target as IDBOpenDBRequest).result) {
                         completedRequests++;
