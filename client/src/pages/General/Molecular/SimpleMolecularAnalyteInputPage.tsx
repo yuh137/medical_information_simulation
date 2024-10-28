8102209d65298fd75b6891fe06abac7a5350d182
Colby-Coffman <colbycoffman20@gmail.com>
2024-10-28 10:29:39 -0500

    W

--- client/src/pages/General/Molecular/SimpleMolecularAnalyteInputPage.tsx
+++ client/src/pages/General/Molecular/SimpleMolecularAnalyteInputPage.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useRef, useMemo } from 'react';
-import { MolecularQCTemplateBatch, MolecularQCTemplateBatchAnalyte, QualitativeMolecularQCTemplateBatchAnalyte, QualitativeViralLoadRangeMolecularQCTemplateBatchAnalyte } from  '../../../utils/indexedDB/IDBSchema';
+import { MolecularQCTemplateBatch, MolecularQCTemplateBatchAnalyte, QualitativeMolecularQCTemplateBatchAnalyte, QualitativeViralLoadRangeMolecularQCTemplateBatchAnalyte, ReportType } from  '../../../utils/indexedDB/IDBSchema';
 import NavBar from '../../../components/NavBar';
 import MolecularAnalyte from '../../../components/MolecularAnalyte';
 
