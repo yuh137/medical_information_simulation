184326fbfd51abe511dbf80f87e7f1ce2e552dd3
joshduplaa <joshuaduplaa@outlook.com>
2024-10-28 12:42:45 -0500

    added base sign up component in register page

--- clientReactApp-SD/src/pages/RegisterPage.jsx
+++ clientReactApp-SD/src/pages/RegisterPage.jsx
@@ -1,6 +1,7 @@
 
 import { useLocation, Link } from 'react-router-dom'; // Import Link
 import './pageStyles/splashPage.css';
+import SignUp from '../components/SignUp';
 
 export default function RegisterPage(){
   return(
@@ -11,6 +12,7 @@ export default function RegisterPage(){
         <div>
           register page. Add account to database
         </div>
+        <SignUp></SignUp>
         <div><Link to="/" style={{ textDecoration: 'none', color: 'inherit', fontSize: '2rem', fontWeight: 'bold' }}>Go To SplashPage</Link></div>
     </>
   );
