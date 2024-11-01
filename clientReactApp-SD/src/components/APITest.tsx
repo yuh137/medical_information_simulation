a61c77b21eeb35fc6aaedc36d614e069d83658d9
joshduplaa <joshuaduplaa@outlook.com>
2024-11-01 00:38:33 -0500

    fixedStylesfor APITest.tsx component

--- clientReactApp-SD/src/components/APITest.tsx
+++ clientReactApp-SD/src/components/APITest.tsx
@@ -18,7 +18,6 @@ import addData from '../util/indexedDB/addData';
 // Import types
 import { QCTemplateBatch } from '../util/indexedDB/IDBSchema';
 
-// Styled Card component
 const Card = styled(MuiCard)(({ theme }) => ({
   display: 'flex',
   flexDirection: 'column',
@@ -30,10 +29,15 @@ const Card = styled(MuiCard)(({ theme }) => ({
   [theme.breakpoints.up('sm')]: {
     maxWidth: '450px',
   },
-  backgroundColor: theme.palette.mode === 'dark' ? '#457A64' : '#607D8B',
+  backgroundColor: theme.palette.mode === 'dark' ? '#457A64' : '#607D8B', // Darker steel blue for dark mode, lighter steel blue for light mode
   boxShadow:
     'hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px',
+  ...theme.applyStyles('dark', {
+    boxShadow:
+      'hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px',
+  }),
 }));
+
 
 const SignInContainer = styled(Stack)(({ theme }) => ({
   height: 'calc((1 - var(--template-frame-height, 0)) * 100dvh)',
@@ -48,12 +52,17 @@ const SignInContainer = styled(Stack)(({ theme }) => ({
     position: 'absolute',
     zIndex: -1,
     inset: 0,
-    backgroundColor: theme.palette.mode === 'dark' ? '#457A64' : '#607D8B',
+    backgroundColor: theme.palette.mode === 'dark' ? '#457A64' : '#607D8B', // Darker steel blue for dark mode, lighter steel blue for light mode
     backgroundImage:
       'radial-gradient(ellipse at 50% 50%, hsl(210, 100%, 97%), hsl(0, 0%, 100%))',
     backgroundRepeat: 'no-repeat',
+    ...theme.applyStyles('dark', {
+      backgroundImage:
+        'radial-gradient(at 50% 50%, hsla(210, 100%, 16%, 0.5), hsl(220, 30%, 5%))',
+    }),
   },
 }));
+
 
 // API URL from environment
 const apiUrl = import.meta.env.VITE_API_URL;
