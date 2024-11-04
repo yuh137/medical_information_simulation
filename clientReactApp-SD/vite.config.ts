48db1943cd27551df78053cb8280a2f36c9ac53a
joshduplaa <joshuaduplaa@outlook.com>
2024-11-04 07:50:50 -0600

    deployed front end on firebase

--- clientReactApp-SD/vite.config.ts
+++ clientReactApp-SD/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: './',
   plugins: [react()],
   server: {
     port: 3000, // change this to your desired port, e.g., 3000
